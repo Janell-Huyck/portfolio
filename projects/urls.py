2bd09161343ed8c28ef7e7b15e28cbe82643cb39
Janell.Huyck <Janell.Huyck@outlook.com>
2020-08-03 17:58:10 -0400

    redesigned menu to be responsive.  broke index.html into multiple views and pages

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,8 @@ from projects import views
 
 urlpatterns = [
     path("", views.index, name="home"),
+    path("featured", views.featured, name="featured"),
+    path("frontend", views.front_end, name="frontend"),
+    path("backend", views.back_end, name="backend"),
     path("<slug:slug>", views.projectDetail, name="project_detail"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,34 +1,47 @@
 from django.shortcuts import render
 from projects.models import Project
 
-from projects.display_text import intro1, intro2, intro3, skills
+from projects.display_text import intro1, intro2
 
 
 def index(request):
     profile_image_url = "../static/janell-webcam-no-background.png"
-    featured_projects = Project.objects.filter(is_featured=True)[0]
-    front_end_projects = (
-        Project.objects.filter(language__in=["RR", "RE", "JS"])
-        .filter(is_featured=False)
-        .order_by("display_order")
-    )
-    back_end_projects = (
-        Project.objects.filter(language__in=["DJ", "PY"])
-        .filter(is_featured=False)
-        .order_by("display_order")
-    )
 
     context = {
         "profile_image_url": profile_image_url,
         "intro1": intro1,
         "intro2": intro2,
-        "intro3": intro3,
-        "skills": skills,
-        "featured": featured_projects,
-        "back_end_projects": back_end_projects,
-        "front_end_projects": front_end_projects,
     }
     html = "index.html"
+    return render(request, html, context)
+
+
+def featured(request):
+    html = "featured.html"
+    featured = Project.objects.filter(is_featured=True)[0]
+    context = {"featured": featured}
+    return render(request, html, context)
+
+
+def front_end(request):
+    html = "frontend.html"
+    projects = (
+        Project.objects.filter(language__in=["RR", "RE", "JS"])
+        .filter(is_featured=False)
+        .order_by("display_order")
+    )
+    context = {"projects": projects}
+    return render(request, html, context)
+
+
+def back_end(request):
+    html = "backend.html"
+    projects = (
+        Project.objects.filter(language__in=["DJ", "PY"])
+        .filter(is_featured=False)
+        .order_by("display_order")
+    )
+    context = {"projects": projects}
     return render(request, html, context)
 
 
