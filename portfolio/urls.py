0040106a87c0f21e17fd0cab070374d40a49559b
Janell.Huyck <Janell.Huyck@outlook.com>
2020-08-18 22:14:32 -0400

    added urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) to portfolio.urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
     path("blog/", include("blog.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = error404
 handler500 = error500
